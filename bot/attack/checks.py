2c57a1409e029308a31bed363af700ea934b895a
Dzejeu <dzejeu@gmail.com>
2020-11-06 19:14:04 +0100

    checking if char is attacking

--- bot/attack/checks.py
+++ bot/attack/checks.py
@@ -1,39 +1,30 @@
-from python_imagesearch.imagesearch import imagesearcharea
-from pyautogui import leftClick, moveTo
+from pyautogui import screenshot, alert
 from bot.cords.point import Point
 
 
-MONSTERS_BARS_AREA = (Point(530, 275), Point(715, 425))
-BATTLE_AREA = (Point(1245, 15), Point(1275, 270))
-FIRST_TARGET_ON_BATTLE = Point(1262, 27)
-NEUTRAL_POS = Point(200, 200)
+BATTLE_AREA_START = Point(1252, 15)
+BATTLE_AREA_END = Point(1253, 160)
+BATTLE_AREA_REGION = (
+    *BATTLE_AREA_START,
+    BATTLE_AREA_END.x - BATTLE_AREA_START.x,
+    BATTLE_AREA_END.y - BATTLE_AREA_START.y
+)
 
 
-def is_monster_around():
-    hp_bar_tpl = "templates/hp_bar.png"
-    found_cords = Point(
-        *imagesearcharea(
-            hp_bar_tpl,
-            MONSTERS_BARS_AREA[0].x,
-            MONSTERS_BARS_AREA[0].y,
-            MONSTERS_BARS_AREA[1].x,
-            MONSTERS_BARS_AREA[1].y,
-            precision=0.9,
-        )
-    )
-    return found_cords != Point(-1, -1)
+def _is_red(color, threshold=30):
+    return abs(255 - color[0]) < threshold and color[1] < threshold and color[2] < threshold
 
 
 def is_currently_attacking():
-    attack_mark = "templates/attack_battle.png"
-    found_cords = Point(
-        *imagesearcharea(
-            attack_mark,
-            BATTLE_AREA[0].x,
-            BATTLE_AREA[0].y,
-            BATTLE_AREA[1].x,
-            BATTLE_AREA[1].y,
-            precision=0.6,
-        )
-    )
-    return found_cords != Point(-1, -1)
+    battle_ss = screenshot(region=BATTLE_AREA_REGION)
+    colors = battle_ss.getcolors()
+    top_5_colors = sorted(colors, key=lambda x: x[0], reverse=True)[:min(len(colors), 5)]
+    for col in top_5_colors:
+        if _is_red(col[1]):
+            return True
+    return False
+
+
+alert("go")
+
+print(is_currently_attacking())
